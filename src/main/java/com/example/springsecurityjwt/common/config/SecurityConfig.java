4ff8661b191262070fc15349653dad829a359bed
hoosa <janghoosa@gmail.com>
2023-03-21 18:26:55 +0900

    feat: 기본페이지 로그인 설정

---
+++ src/main/java/com/example/springsecurityjwt/common/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.springsecurityjwt.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeRequests(
+            authorize -> authorize.antMatchers("/login")
+                .permitAll() // 로그인 페이지는 모든 사용자에게 허용
+                .anyRequest().authenticated() // 나머지 요청은 인증된 사용자만 허용
+        ).formLogin() // formLogin() 메소드를 사용하여 로그인 폼 생성
+        .defaultSuccessUrl("/member")
+        .permitAll(); // 로그인 페이지는 모든 사용자에게 허용
+
+    return http.build();
+  }
+}
