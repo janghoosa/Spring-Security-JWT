11da375dc3ae36adace2fbfa1b070aadbe112dc2
hoosa <janghoosa@gmail.com>
2023-03-30 18:09:53 +0900

    feat: JWT 토큰 검증 추가

--- src/main/java/com/example/springsecurityjwt/common/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjwt/common/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.springsecurityjwt.common.config;
 import com.example.springsecurityjwt.common.config.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,15 +17,16 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.addFilterBefore(new JwtAuthenticationFilter(), BasicAuthenticationFilter.class)
+    http.addFilterBefore(
+        new JwtAuthenticationFilter(),
+            BasicAuthenticationFilter.class)
         .csrf().disable()
         .authorizeRequests(
-            authorize -> authorize.antMatchers("/login",
-                    "/members**")
+            authorize -> authorize.antMatchers("/login", "/members**", "/auth/login**")
                 .permitAll() // 로그인 페이지는 모든 사용자에게 허용
-                .anyRequest().authenticated() // 나머지 요청은 인증된 사용자만 허용
-        )
-        .formLogin() // formLogin() 메소드를 사용하여 로그인 폼 생성
+                .anyRequest()
+                .authenticated() // 나머지 요청은 인증된 사용자만 허용
+        ).formLogin() // formLogin() 메소드를 사용하여 로그인 폼 생성
         .defaultSuccessUrl("/members")
         .permitAll(); // 로그인 페이지는 모든 사용자에게 허용
 
@@ -34,4 +37,11 @@ public class SecurityConfig {
   public BCryptPasswordEncoder encodePassword() {
     return new BCryptPasswordEncoder();
   }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
 }
